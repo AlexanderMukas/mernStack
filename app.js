68d732c320d6b6f4502473f252f2ff1ba38b7e25
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-16 00:18:49 +0300

    mongo and config commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('App!');
+// import Express
+const express = require('express');
+//from dir config
+const config = require('config');
+//mongodb
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+// connect to MongoDB
+async function start(){
+    try {
+
+    } catch (e){
+
+    }
+}
+
+start()
+
+const PORT = config.get("port") || 5000;                        // if undefined, then port = 5000
+//const port = 5000;
+app.listen(PORT, () => {
+    console.log(`App has been started on port ${PORT}...`);
+});
+
