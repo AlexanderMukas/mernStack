5121565b1db46750cf65485e71ab2113545ec345
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-18 00:42:14 +0300

    add login route commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 const router = Router();
 const User = require('../models/User');
@@ -47,11 +49,47 @@ router.post(
 
 })
 //path : /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+    '/login', 
+    // need validation on server now
+    [
+        check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+        check('password', 'Введите пароль').exists()
+    ],
+    async (req, res) => {
     try {
-
-    } catch (e) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty) {
+            return res.status(400).json({ 
+                errors: errors.array(),
+                message: 'Некорректные данные при сходе в систему'
+            });
+        }
         
+        const {email, password} = req.body;    // get from frontend
+        // существует ли пользователь в базе
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({ message: 'Пользователь не найден!'})
+        };
+        // if match - совпадают ли пароль в базе от внесеного пользователем
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: 'Неверный пароль, пробуйте снова!'})
+        }
+        // юзер такой есть и пароль верный - надо авторизировать!
+        // так как у нас будет SPA приложение, авторизация будет через token - jsonwebtoken
+        const token = jwt.sign(
+            { userId: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: '1h'}       // one hour life token
+        );
+        // по умолчанию статус - 200, успех
+        res.json({ token, userId: user.id})
+        
+    } catch (e) {
+        res.status(500).json({message : "Something went wrong..."});
+
     }
 })
 
