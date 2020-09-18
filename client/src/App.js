15293ef7ab052daeded07332f7dffc876683ce34
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-19 00:34:06 +0300

    some commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import 'materialize-css';
+
 
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello World!
-      </h1>
+    <div className="container">
+      <h1>Hello</h1>
     </div>
-    
   );
 }
 
