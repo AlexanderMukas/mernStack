e6a4f34726673f7382dcde4cbe1f5859914bdb12
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-21 23:51:15 +0300

    auth commit

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 // страница авторизации
+// используем materialyze
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Сократи ссылку</h1>
+
+            </div>
         </div>
     );
 }
