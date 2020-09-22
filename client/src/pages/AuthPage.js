2b148c232f10549d44ce8d3981911c90eca6679c
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-22 23:58:32 +0300

    editing of Auth page commit

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -7,15 +7,34 @@ export const AuthPage = () => {
         <div className="row">
             <div className="col s6 offset-s3">
                 <h1>Сократи ссылку</h1>
-                <div className="card blue darken-1">
+                <div className="card teal darken-1">
                     <div className="card-content white-text">
                         <span className="card-title">Authorization</span>
                         <div>
+                            <div className="input-field">
+                                <input 
+                                    placeholder="Input email" 
+                                    id="email" 
+                                    type="text" 
+                                    name="email"
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input 
+                                    placeholder="Enter password" 
+                                    id="password" 
+                                    type="password"
+                                    name="password" 
+                                />
+                                <label htmlFor="password">password</label>
+                            </div>
 
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4">Sign in</button>
+                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Sign in</button>
                         <button className="btn grey lighten-1 black-text">Register</button>
                     </div>
                 </div>
